ce14861accbb790d52d455969a27e1fd86d20186
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-02-13 11:12:06 +0100

    refactor to new component for handling fish modal

--- src/fish/FishModal.vue
+++ src/fish/FishModal.vue
@@ -1,9 +1,20 @@
 <template>
-  <article>
-    <h2>{{ fish.name }}</h2>
-    <img :src="fish.image.src" :alt="fish.image.alt" />
-    {{ removeTags(fish.biology) }}
-  </article>
+  <div class="overlay">
+    <article @focusout="closeModal" tabindex="0" ref="modal">
+      <div class="card">
+        <img class="card-img-top" :src="fish.image.src" :alt="fish.image.alt" />
+        <div class="card-body">
+          <h2 class="card-title">{{ fish.name }}</h2>
+          <p class="card-text">{{ removeTags(fish.biology) }}</p>
+        </div>
+      </div>
+    </article>
+    <!-- <article @focusout="closeModal" tabindex="0" ref="modal">
+      <h2>{{ fish.name }}</h2>
+      <img :src="fish.image.src" :alt="fish.image.alt" />
+      <p>{{ removeTags(fish.biology) }}</p>
+    </article> -->
+  </div>
 </template>
 
 <script>
@@ -11,8 +22,8 @@
     emits: {
       closeModal: 'closeModal'
     },
-    created() {
-      // this.closeModal()
+    mounted() {
+      this.$refs.modal.focus()
     },
     props: {
       fish: {
@@ -30,11 +41,38 @@
         else str = str.toString()
         return str.replace(/(<([^>]+)>)/gi, '')
       }
-    },
-    watch: {
-      // Kan kolla om man klickar utanför
     }
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @import '../../assets/main.scss';
+
+  .overlay {
+    position: absolute;
+    width: 100vw;
+    min-height: calc(10rem + 100vh);
+    background-color: #010101bb;
+    z-index: 1;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  article {
+    background-color: $light;
+    position: absolute;
+    top: calc(10rem);
+    left: calc(50%);
+    transform: translate(-50%, 0);
+    width: 50%;
+    z-index: 2;
+    padding: 16px;
+  }
+
+  // img {
+  //   width: 175px;
+  //   height: 175px;
+  //   max-width: 100%;
+  // }
+</style>
