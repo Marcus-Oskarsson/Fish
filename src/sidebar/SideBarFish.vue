3d183c0686f4cea9e5d5875252d13c50d1290be7
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-02-13 11:15:20 +0100

    adds sidebar

--- src/sidebar/SideBarFish.vue
+++ src/sidebar/SideBarFish.vue
@@ -1,24 +1,77 @@
 <template>
-  <aside class="col-sm-3 col-12 overflow-scroll">
-    <pre>
+  <aside class="col-sm-2 col-12 overflow-scroll">
+    <h2>Fiskar</h2>
+    <!-- <pre>
       {{ lastTenFishList }}
-    </pre>
+    </pre> -->
+    <div class="fish-content" v-for="fish in lastTenFishList" :key="fish.id">
+      <p>{{ fish.name }}</p>
+      <img
+        :src="fish?.image?.src"
+        :alt="fish?.image?.alt"
+        class="small-fish-img"
+      />
+      <button class="btn" @click="openModal(fish)" type="button">
+        Läs mer
+      </button>
+    </div>
   </aside>
+  <FishModal
+    @close-modal="closeModal"
+    v-if="isModalOpen && selected"
+    :fish="selected"
+  />
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
 
+  import FishModal from '../fish/FishModal.vue'
+
   export default {
+    components: {
+      FishModal
+    },
     computed: {
       ...mapGetters({
         lastTenFishList: 'catchedFish/lastTenFish'
       })
     },
     data() {
-      return {}
+      return {
+        isModalOpen: false,
+        selected: null
+      }
+    },
+    methods: {
+      closeModal() {
+        this.isModalOpen = false
+      },
+      openModal(fish) {
+        this.isModalOpen = true
+        this.selected = fish
+      }
     }
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @import '../../assets/main.scss';
+
+  aside {
+    background-color: rgb(48, 75, 148);
+    padding: 24px 16px;
+  }
+
+  .fish-content {
+    margin-bottom: 16px;
+  }
+
+  h2,
+  p {
+    color: $light;
+  }
+  .small-fish-img {
+    width: 75px;
+  }
+</style>
