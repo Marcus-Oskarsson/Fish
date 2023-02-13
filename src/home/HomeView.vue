73942eb4df8d8b440893fe923af0ffe3ce7595b5
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-02-13 11:13:11 +0100

    adds input for name and dynamic navigation

--- src/home/HomeView.vue
+++ src/home/HomeView.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="container-fluid main image" :style="cssImageProps">
-    <!-- <div
-    class="container-fluid main image"
-    :style="{ backgroundImage: 'url(' + image + ')' }"
-  > -->
-    <!-- <img :src="backgroundImage" alt="asd" /> -->
-    <RouterLink class="btn btn-lg btn-success link" to="/fish">
-      Fiska!
-    </RouterLink>
+    <form @submit.prevent="navigate">
+      <label for="name">Ditt namn:</label>
+      <input
+        class="form-control"
+        id="name"
+        placeholder="Santiago"
+        required
+        type="text"
+        v-model="name"
+      />
+      <p class="font-italic" v-if="typing">Skriver...</p>
+      <button class="btn btn-lg btn-success col-12 link" type="submit">
+        Börja fiska!
+      </button>
+      <!-- <RouterLink class="btn btn-lg btn-primary col-12 link" to="/fish">
+        Kasta loss
+      </RouterLink> -->
+    </form>
   </div>
 </template>
 
@@ -22,21 +32,46 @@
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
           backgroundSize: 'cover'
-        }
+        },
+        name: null,
+        typing: false
+      }
+    },
+    methods: {
+      navigate() {
+        this.$store.commit('user/addUser', this.name)
+        this.$router.push('/fish')
+      }
+    },
+    watch: {
+      name(newName, oldName) {
+        this.typing = newName && newName !== oldName
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  @import '../../assets/main.scss';
   .main {
     display: flex;
     min-height: 100vh;
   }
 
   .main {
-    .link {
+    form {
+      background-color: #010101aa;
       margin: auto;
+      padding: 48px 36px;
+
+      *:not(:last-child) {
+        margin-bottom: 24px;
+      }
     }
+  }
+
+  label,
+  p {
+    color: $light;
   }
 </style>
