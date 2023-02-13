58e3f18ba02553f9242249c7178334b34191f48d
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-02-13 11:13:32 +0100

    adds profile route

---
+++ src/profile/ProfileView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Välkommen, {{ $store.state.user.name }}</h1>
+  <pre>id: {{ $route.params.id }}</pre>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped></style>
